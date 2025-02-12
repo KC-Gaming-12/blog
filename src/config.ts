a4d8781821c68195cefcfa711c24a8a7bce36476
Tony <kcgaming@lifesteal.team>
2025-02-11 17:19:36 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -64,7 +64,7 @@ export const profileConfig: ProfileConfig = {
     },
     {
       name: 'Discord',
-      icon: 'fa6-brands:Discord',
+      icon: 'fa6-brands:discord',
       url: 'https://www.lifesteal.team/Discord',
     },
     {
